3d926c9ee1b6fc46bd1996d07fbe086134b871e2
Your Name <you@example.com>
2023-07-21 18:07:53 +0530

    inital commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:3000/api",
+    origin: "https://frail-seal-earmuffs.cyclic.app/api",
     credentials: true
 }))
 app.use(express.static("public"))
