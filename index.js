ea534aa8fa5d8102f9d053693018fb1f19097e38
Your Name <you@example.com>
2023-07-21 14:50:24 +0530

    intial commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://driving-6i7uwt83y-manoj509.vercel.app",
     credentials: true
 }))
 app.use(express.static("public"))
